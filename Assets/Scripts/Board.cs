7d8c00987eeb5595ce331bf9933af1d4e59b7115
KANG SOO IN <ks00in213@gmail.com>
2024-03-06 13:24:55 +0900

    feat : Make Puzzles

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+// 게임 시작 시 보드 셋팅
+public class Board : MonoBehaviour
+{
+	public int width;
+	public int height;
+
+	public GameObject BackgroundTilePrefab;
+
+	void Start()
+	{
+		BoardSetUp();
+	}
+
+	void BoardSetUp()
+	{
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{ 
+				Vector2 pos = new Vector2(x, y);
+				GameObject backgroundTile = Instantiate(BackgroundTilePrefab, pos, Quaternion.identity);
+				backgroundTile.transform.parent = transform;
+				backgroundTile.name = "BackgroundTile - " + x + ", " + y;
+			}
+		}
+	}	
+}
