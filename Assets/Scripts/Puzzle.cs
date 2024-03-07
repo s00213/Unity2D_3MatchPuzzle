64ef0520fbd5bd41b65d80268a79d9969be773e4
s00213 <ks00in213@gmail.com>
2024-03-08 04:27:29 +0900

    feat : Move Puzzles

--- Assets/Scripts/Puzzle.cs
+++ Assets/Scripts/Puzzle.cs
@@ -8,10 +8,11 @@ public class Puzzle : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public Vector2Int posIndex;
     public Board board;
 
-    Vector2 firstTouchPosition;
-    Vector2 finalTouchPosition;
+    Vector2 firstTouchPos;
+    Vector2 finalTouchPos;
 
     float swipeAngle = 0;
+    Puzzle otherPuzzle;
 
     public void PuzzleSetUp(Vector2Int pos, Board _board)
     {
@@ -19,28 +20,66 @@ public class Puzzle : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         board = _board;
     }
 
+    // 포인터가 오브젝트 위에서 눌렸을 때 호출됨
     public void OnPointerDown(PointerEventData eventData)
     {
-        firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		CalculateSwipeAngle();
+        firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Debug.Log("Down");
 	}
 
-    public void OnPointerUp(PointerEventData eventData)
+	// 포인터를 뗄 때 호출됨
+	public void OnPointerUp(PointerEventData eventData)
     {
-        finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        finalTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         CalculateSwipeAngle();
     }
 
     void CalculateSwipeAngle()
     {
-        swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x);
+        swipeAngle = Mathf.Atan2(finalTouchPos.y - firstTouchPos.y, finalTouchPos.x - firstTouchPos.x);
         swipeAngle = swipeAngle * 180 / Mathf.PI;
         Debug.Log(swipeAngle);
-    }
+
+
+        if (Vector3.Distance(firstTouchPos, finalTouchPos) > 0.5f)
+        {
+			MovePuzzles();
+		}
+	}
 
     void MovePuzzles()
     {
+		// 오른쪽 방향으로 교환할 때
+		if (swipeAngle < 45 && swipeAngle > -45 && posIndex.x < board.width - 1)
+		{
+			otherPuzzle = board.allPuzzles[posIndex.x + 1, posIndex.y];
+			otherPuzzle.posIndex.x--;
+			posIndex.x++;
+		}
+		// 왼쪽 방향으로 교환할 때
+		else if (swipeAngle > 135 || swipeAngle < -135 && posIndex.x > 0)
+		{
+			otherPuzzle = board.allPuzzles[posIndex.x - 1, posIndex.y];
+			otherPuzzle.posIndex.x++;
+			posIndex.x--;
+		}
+		// 위쪽 방향으로 교환할 때
+		else if (swipeAngle > 45 && swipeAngle <= 135 && posIndex.y < board.height - 1)
+		{
+			otherPuzzle = board.allPuzzles[posIndex.x, posIndex.y + 1];
+			otherPuzzle.posIndex.y--;
+			posIndex.y++;
+		}
+		// 아래쪽 방향으로 교환할 때
+		else if (swipeAngle < -45 && swipeAngle >= -135 && posIndex.y > 0)
+		{
+			otherPuzzle = board.allPuzzles[posIndex.x, posIndex.y - 1];
+			otherPuzzle.posIndex.y++;
+			posIndex.y--;
+		}		
 
-    }
+		board.allPuzzles[posIndex.x, posIndex.y] = this;
+		board.allPuzzles[otherPuzzle.posIndex.x, otherPuzzle.posIndex.y] = otherPuzzle;
+	}
 }
 
